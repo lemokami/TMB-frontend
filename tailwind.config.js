91b7941e26997e6b7eeeb4c54f1d7ba49007b33e
lemokami <jesswinw@gmail.com>
2022-06-26 23:03:49 +0530

    feat: add space mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'space-mono': ['Space Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
