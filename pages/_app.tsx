9cae837523127e3b241567d32b14e0cb44474205
lemokami <jesswinw@gmail.com>
2022-06-26 22:13:46 +0530

    feat: phantom wallet support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { WalletContext } from '../components/WalletContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WalletContext>
+      <Component {...pageProps} />
+    </WalletContext>
+  );
 }
 
 export default MyApp;
