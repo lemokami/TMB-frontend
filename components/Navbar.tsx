d6c4e1e6aae14b00c771f74f33447c417e4c05f0
lemokami <jesswinw@gmail.com>
2022-06-27 15:20:20 +0530

    feat: made the Brand icon lead to home

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,19 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useRouter } from 'next/router';
 import { FC, ReactNode } from 'react';
 
 const Navbar: FC<{ children?: ReactNode; wallet?: boolean }> = ({
   children,
   wallet,
 }) => {
+  const router = useRouter();
   return (
     <div className='p-8 py-4 flex justify-between items-center'>
-      <h3 className='font-bold text-lg'>TrustMeBro</h3>
+      <h3
+        onClick={() => router.push('/')}
+        className='font-bold text-lg cursor-pointer'>
+        TrustMeBro
+      </h3>
       <div className='flex items-center gap-2'>
         {children}
         {wallet && <WalletMultiButton />}
