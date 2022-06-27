3377b7ba19cb34dbffe6056e195d456b4cb72d40
lemokami <jesswinw@gmail.com>
2022-06-28 02:33:51 +0530

    feat: login working with backend

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,48 @@
 import { useAnchorWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useMutation } from 'react-query';
 import Navbar from '../components/Navbar';
+import { AXIOS } from '../helpers/axios';
 
 const Login = () => {
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const [user, setUser] = useState<any>(null);
   const wallet = useAnchorWallet();
   const router = useRouter();
+  const createOrCheckUser = useMutation(
+    () =>
+      AXIOS.post('/signin', {
+        key: wallet?.publicKey.toString(),
+      }),
+    {
+      onSuccess: (data) => {
+        setAuthenticated(true);
+        setUser(data.data);
+      },
+    }
+  );
 
   useEffect(() => {
     if (wallet) {
       // TODO: send signin request
-      // redirect to home
-      router.push('/user');
+      createOrCheckUser.mutate();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [wallet]);
+
+  useEffect(() => {
+    if (authenticated) {
+      sessionStorage.setItem('user', JSON.stringify(user));
+      console.log(user);
+      if (user.completed_profile === false) {
+        router.push('/new/profile');
+      } else {
+        router.push('/');
+      }
+    }
+  }, [authenticated, router, user]);
 
   return (
     <div className='min-h-screen flex flex-col'>
